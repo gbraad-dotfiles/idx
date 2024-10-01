153fc8cdca04c5ab453ec9563e6a8cce3ff64487
Gerard Braad <me@gbraad.nl>
2024-10-01 13:14:58 +0800

    Fix podman and install dotfiles

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,17 +1,20 @@
 { pkgs, ... }: {
   channel = "stable-24.05";
   
- packages = [
-    pkgs.stow
+  packages = [
+    pkgs.tailscale
     pkgs.screen
     pkgs.tmux
     pkgs.zsh
     pkgs.powerline
     pkgs.podman
-    pkgs.tailscale
+    pkgs.stow
+    pkgs.openssh
   ];
   
-  env = {};
+  env = {
+    
+  };
   
   idx = {
     extensions = [
@@ -19,14 +22,37 @@
       "tailscale.vscode-tailscale"
     ];
     workspace = {
-      onCreate = {};
-      onStart = {};
+      onCreate = {
+        # this is to fix Podman: https://github.com/gbraad-devenv/idx/issues/3
+        setup-podman = ''
+          mkdir /etc/containers/
+          cat >> /etc/containers/policy.json <<EOF
+          { "default" : [ { "type": "insecureAcceptAnything"} ]}
+          EOF
+          mkdir /var/tmp
+        '';
+        install-dotfiles = ''
+          git clone https://github.com/gbraad/dotfiles /tmp/dotfiles
+          /tmp/dotfiles/install.sh
+        '';
+      };
+      onStart = {
+        start-tailscale = ''
+          screen -d -m tailscaled \
+            --tun="userspace-networking" \
+            --socket=/var/run/tailscale/tailscaled.sock
+        '';
+        default.openFiles = [];
+      };
     };
     
   };
 
   services = {
     docker.enable = true;
+
+    # this does not work on IDX
     #tailscale.enable = true;
+    #tailscale.interfaceName = "userspace-networking";
   };
 }
